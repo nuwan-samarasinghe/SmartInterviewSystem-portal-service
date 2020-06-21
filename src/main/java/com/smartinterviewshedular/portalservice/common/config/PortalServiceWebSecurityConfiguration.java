ad8643ae99a76a0c851672e57193d4175d711eca
nuwansamarasinghe100@gmail.com <sama@272199360G>
2020-06-22 01:57:19 +0530

    adding cors

--- src/main/java/com/smartinterviewshedular/portalservice/common/config/PortalServiceWebSecurityConfiguration.java
+++ src/main/java/com/smartinterviewshedular/portalservice/common/config/PortalServiceWebSecurityConfiguration.java
@@ -1,10 +1,11 @@
 package com.smartinterviewshedular.portalservice.common.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 
 @Configuration("portalServiceWebSecurityConfiguration")
 public class PortalServiceWebSecurityConfiguration extends ResourceServerConfigurerAdapter {
@@ -13,7 +14,9 @@ public class PortalServiceWebSecurityConfiguration extends ResourceServerConfigu
     public void configure(HttpSecurity http) throws Exception {
         http.csrf()
                 .disable()
+                .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
                 .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "**").permitAll()
                 .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs/**", "/webjars/**").permitAll()
                 .antMatchers("/**").authenticated();
     }
