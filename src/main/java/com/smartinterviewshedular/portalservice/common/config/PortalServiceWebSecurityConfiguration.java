95a004d8a318d77708adc6090588ff9948671352
nuwansamarasinghe100@gmail.com <sama@272199360G>
2020-06-21 23:17:44 +0530

    adding Dockerfile

--- src/main/java/com/smartinterviewshedular/portalservice/common/config/PortalServiceWebSecurityConfiguration.java
+++ src/main/java/com/smartinterviewshedular/portalservice/common/config/PortalServiceWebSecurityConfiguration.java
@@ -1,11 +1,12 @@
 package com.smartinterviewshedular.portalservice.common.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
-@Configuration("portalServiceResourceServerConfigurerAdapter")
+@Configuration("portalServiceWebSecurityConfiguration")
 public class PortalServiceWebSecurityConfiguration extends ResourceServerConfigurerAdapter {
 
     @Override
@@ -13,6 +14,12 @@ public class PortalServiceWebSecurityConfiguration extends ResourceServerConfigu
         http.csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs/**").permitAll();
+                .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs/**", "/webjars/**").permitAll()
+                .antMatchers("/**").authenticated();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.resourceId("portalservice");
     }
 }
